628d9cab1e8ef7bc937551a557a3318b6c61c3b4
AFM project <133870019+mohammad-mohammad-soltani@users.noreply.github.com>
2024-03-21 16:30:13 +0330

    Update db_config.php

--- panel/config/db_config.php
+++ panel/config/db_config.php
@@ -1,8 +1,8 @@
 <?php
 define('server', 'localhost');
-define('username', 'root');
-define('password', '');
-define('db', 'afm');
+define('username', 'phpmyadmin');
+define('password', 'sA*01569');
+define('db', 'afm_db');
 define('server2', server);
 define('username2', username);
 define('password2', password);
